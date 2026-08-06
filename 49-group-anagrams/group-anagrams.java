d6c09b86e93e978aacd5d4ae67e73a85b3fff0b9
Divyansh Yadav <dyadav.9648@gmail.com>
2026-08-07 00:17:42 +0530

    Time: 6 ms (99.24%) | Memory: 49.8 MB (32.53%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,32 +1,21 @@
 import java.util.*;
 
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        // Agar array khali hai, toh empty list return karo
-        if (strs == null || strs.length == 0) {
+    public List<List<String>> groupAnagrams(String[] strs){
+        if (strs==null || strs.length ==0){
             return new ArrayList<>();
         }
-        
-        // Key: Sorted String (e.g. "aet")
-        // Value: List of original Anagrams (e.g. ["eat", "tea", "ate"])
-        Map<String, List<String>> map = new HashMap<>();
-        
-        for (String s : strs) {
-            // 1. String ko character array mein badalo aur sort karo
-            char[] charArray = s.toCharArray();
+        Map <String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] charArray =s.toCharArray();
             Arrays.sort(charArray);
-            
-            // 2. Sorted character array ko wapas String (Key) banao
-            String sortedKey = String.valueOf(charArray);
-            
-            // 3. Agar key map mein nahi hai, toh nayi ArrayList bana do
-            map.putIfAbsent(sortedKey, new ArrayList<>());
-            
-            // 4. Asli word ko us Key ki list mein add kar do
+            String sortedKey= String.valueOf(charArray);
+            map.putIfAbsent(sortedKey,new ArrayList<>());
             map.get(sortedKey).add(s);
         }
-        
-        // HashMap ki saari lists ko combine karke return kar do
+
         return new ArrayList<>(map.values());
     }
-}
+
+
+}        
