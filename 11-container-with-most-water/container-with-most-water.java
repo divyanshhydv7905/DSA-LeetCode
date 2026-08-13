eb397913c1360bada79cc0150dd10fe267dd7edb
Divyansh Yadav <dyadav.9648@gmail.com>
2026-08-14 01:02:31 +0530

    Time: 5 ms (82.94%) | Memory: 77.5 MB (29.55%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -5,15 +5,11 @@ class Solution {
         int maxWater = 0;
         
         while (left < right) {
-            // Choti height container ki height decide karegi
+           
             int h = Math.min(height[left], height[right]);
             int width = right - left;
-            
-            // Current area calculate karke maxWater update karo
             int currentWater = h * width;
             maxWater = Math.max(maxWater, currentWater);
-            
-            // Jo line choti hai, use badhao/ghatao
             if (height[left] < height[right]) {
                 left++;
             } else {
